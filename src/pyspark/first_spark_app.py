62ca05b10f04be736d4e566d888043bf8914d494
upendrakumar <upendrarahul27@gmail.com>
2026-08-11 16:31:52 +0530

    Format PySpark files with Black

--- src/pyspark/first_spark_app.py
+++ src/pyspark/first_spark_app.py
@@ -1,16 +1,8 @@
 from pyspark.sql import SparkSession
 
-spark = (
-    SparkSession.builder
-    .appName("Read Employee CSV")
-    .getOrCreate()
-)
+spark = SparkSession.builder.appName("Read Employee CSV").getOrCreate()
 
-df = (
-    spark.read
-    .option("header", True)
-    .csv("datasets/employees.csv")
-)
+df = spark.read.option("header", True).csv("datasets/employees.csv")
 
 print("DataFrame Created")
 
--- src/pyspark/test1.py
+++ src/pyspark/test1.py
@@ -1,16 +1,8 @@
 from pyspark.sql import SparkSession
 
-spark = (
-    SparkSession.builder
-    .appName("Read Employee CSV")
-    .getOrCreate()
-)
+spark = SparkSession.builder.appName("Read Employee CSV").getOrCreate()
 
-df = (
-    spark.read
-    .option("header", True)
-    .csv("datasets/employees.csv")
-)
+df = spark.read.option("header", True).csv("datasets/employees.csv")
 
 df.filter(df.salary > 50000)
 df.printSchema()
